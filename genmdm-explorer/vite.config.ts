840ae4f1de08c1a002642daa566f4b562f9510c0
eggstoastbacon <eggstoastbacon@gmail.com>
2025-08-11 14:28:57 -0700

    Update vite.config.ts

--- genmdm-explorer/vite.config.ts
+++ genmdm-explorer/vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// User/Org site: base must be '/'
+// User/Org site: base must bee '/'
 export default defineConfig({
   plugins: [react()],
   base: '/',
