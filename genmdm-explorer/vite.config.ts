b23045107dd32c130c1af22ce0b5d09f5f8fb0b8
eggstoastbacon <eggstoastbacon@gmail.com>
2025-08-11 14:34:04 -0700

    Add files via upload

--- genmdm-explorer/vite.config.ts
+++ genmdm-explorer/vite.config.ts
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// User/Org site: base must bee '/'
+// Project under user site subpath: /genmdm-explorer/
 export default defineConfig({
   plugins: [react()],
-  base: '/',
+  base: '/genmdm-explorer/',
 })
